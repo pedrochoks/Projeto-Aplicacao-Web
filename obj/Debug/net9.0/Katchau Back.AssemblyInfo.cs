99d674b6f7965d49c38e2180002afff2fac3fe29
Pedro Augusto <gomespedroagusto85@gmail.com>
2025-12-09 08:52:29 -0300

    update

--- obj/Debug/net9.0/Katchau Back.AssemblyInfo.cs
+++ obj/Debug/net9.0/Katchau Back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Katchau Back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2c4527658b0e7e5420078aa42821ed4a630515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2671b7084349ff3745886b25c8aa39c1bf509d89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Katchau Back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Katchau Back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
